4d71d4c768d9360431c510436216b3637a8ee3eb
Rodrigo Viturino <viturino_souza@outlook.com>
2018-09-12 00:27:49 -0300

    Iniciando projeto

---
+++ Class/Sql.php
@@ -0,0 +1,49 @@
+<?php
+
+class Sql extends PDO {
+
+    // Atributo
+    private $conexao;
+
+    //  Vai nascer já com está função
+    public function __construct() {
+        $this->conexao = new PDO("mysql:dbname=dbphp7;host=localhost", "root","");
+    }
+
+    // Método para receber varios parametros/resultado
+    private function setParams($statment, $parameters = array()) {
+
+        foreach ($parameters as $key => $value ) {
+            $this->setParam($key, $value);
+        }
+    }
+
+    private function setParam($statment, $key, $value) {
+        $statment->bindParam($key, $value);
+    }
+
+    public function query($rawQuery, $params = array()) {
+
+        $stmt = $this->conexao->prepare($rawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();  
+
+        return $stmt;
+    }
+
+
+    public function select($rawQuery, $params = array()):array {
+
+        $stmt = $this->query($rawQuery, $params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        
+    }
+
+
+}
+
+?>
